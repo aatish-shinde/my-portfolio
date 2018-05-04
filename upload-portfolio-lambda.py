7bf641a00cc431036289ade39d876c53f156beba
Atish Shinde <aatish.shinde@gmail.com>
2018-05-04 11:56:17 -0400

    Use bucket name and object key from codepipeline and notify when codepipeline job done

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -10,6 +10,20 @@ def lambda_handler(event, context):
     topic = sns.Topic('arn:aws:sns:us-east-1:791984576330:deployPortfolioTopic')
 
     try:
+        job = event.get("CodePipeline.job")
+        
+        location = {
+            "bucketName": 'portfoliobuild.atish.shinde.info',
+            "objectKey": 'portfoliobuild.zip'
+        }
+        
+        if job:
+                for artifact in job["data"]["inputArtifacts"]:
+                    if artifact["name"] == "MyAppBuild":
+                        location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
+        
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
         
         #s3 = boto3.resource('s3')
@@ -19,11 +33,11 @@ def lambda_handler(event, context):
         #for obj in portfolio_bucket.objects.all(): print obj.key
         #portfolio_bucket.download_file('index.html', 'c:\code\index.html')
         
-        build_bucket = s3.Bucket('portfoliobuild.atish.shinde.info')
-        #build_bucket.download_file('portfoliobuild.zip', 'c:\code\portfoliobuild.zip')
+        build_bucket = s3.Bucket(location["bucketName"])
+        #build_bucket.download_file(, 'c:\code\portfoliobuild.zip')
         
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
         
         #with zipfile.ZipFile(portfolio_zip) as myzip:
         #    for nm in myzip.namelist():
@@ -42,6 +56,10 @@ def lambda_handler(event, context):
     
         print 'Job done'
         topic.publish(Subject="Portfolio deployed", Message="Portfolio deployed successfully!")
+        
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId = job["id"])
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully!")
         raise
